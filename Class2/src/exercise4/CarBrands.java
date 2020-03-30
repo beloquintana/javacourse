cda0d99589307ad3ea8493a9a4b6849f4faac8f1
beloquintana <belo.quintana@gmail.com>
2020-03-30 11:39:07 -0300

    add exercise 4 and 5, class 2

---
+++ Class2/src/exercise4/CarBrands.java
@@ -0,0 +1,24 @@
+package exercise4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarBrands {
+    public static void main(String arg[]){
+        List<String> list = new ArrayList<String>();
+        list.add("BMW");
+        list.add("VW");
+        list.add("Chevrolet");
+        list.add("Lada");
+
+        System.out.println("Marcas de autos en venta:");
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static int sumCalculator(int number1, int number2){
+        int result = number1 + number2;
+        return result;
+    }
+}
---
+++ Class2/src/exercise5/Greetings.java
@@ -0,0 +1,16 @@
+package exercise5;
+
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String arg[]){
+        System.out.println("Entre su nombre:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        greetUser(name);
+    }
+    
+    public static void greetUser(String name){
+        System.out.println("Hola " + name);
+    }
+}
